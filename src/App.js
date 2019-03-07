ec1d39993d84cdbb6abdb21484e3655588d7a2a5
vmakarov <clevertechru@users.noreply.github.com>
2019-03-07 13:02:05 +0300

    buttons

--- src/App.js
+++ src/App.js
@@ -1,19 +1,68 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
-import Button from '@material-ui/core/Button'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import tileData from './tileData';
+import Button from '@material-ui/core/Button';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <Button variant="contained" color="primary">
-          Hello World
-        </Button>
-        <br />
-        <Button variant="contained" color="primary">
-        Hello World
-        </Button>
-      </div>
-    )
+const styles = theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    width: 300,
+    height: 450,
+  },
+  button: {
+    width: "100%",
   }
+});
+
+/**
+ * The example data is structured as follows:
+ *
+ * import image from 'path/to/image.jpg';
+ * [etc...]
+ *
+ * const tileData = [
+ *   {
+ *     img: image,
+ *     title: 'Image',
+ *     author: 'author',
+ *     cols: 2,
+ *   },
+ *   {
+ *     [etc...]
+ *   },
+ * ];
+ */
+function ImageGridList(props) {
+  const { classes } = props;
+
+  return (
+    <div className={classes.root}>
+      <GridList cellHeight={70} className={classes.gridList} cols={1}>
+        {tileData.map(tile => (
+          <GridListTile key={tile.img} cols={tile.cols || 1}>
+            <Button variant="contained" className={classes.button}>
+              {tile.title}
+              <br />
+              {tile.description}
+            </Button>
+          </GridListTile>
+        ))}
+      </GridList>
+    </div>
+  );
 }
+
+ImageGridList.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ImageGridList);
---
+++ src/tileData.js
@@ -0,0 +1,30 @@
+const tileData = [
+  {
+    img: '/static/images/grid-list/breakfast.jpg',
+    title: 'Breakfast',
+    description: 'Breakfast description',
+    cols: 1,
+    featured: true,
+  },
+  {
+    img: '/static/images/grid-list/burgers.jpg',
+    title: 'Tasty burger',
+    description: 'Tasty burger description',
+    cols: 1,
+  },
+  {
+    img: '/static/images/grid-list/camera.jpg',
+    title: 'Camera',
+    description: 'Camera description',
+    cols: 1,
+  },
+  {
+    img: '/static/images/grid-list/morning.jpg',
+    title: 'Morning',
+    description: 'Morning description',
+    cols: 1,
+    featured: true,
+  }
+]
+
+export default tileData
